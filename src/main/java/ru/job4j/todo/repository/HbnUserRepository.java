bbf66e83ad492bd77c88e619b8df26d8aefbd228
pavelberzkaln <p.berzkaln@gmail.com>
2023-03-23 23:35:01 +0300

    6. Лямбды и шаблон Command [#49295]

--- src/main/java/ru/job4j/todo/repository/HbnUserRepository.java
+++ src/main/java/ru/job4j/todo/repository/HbnUserRepository.java
@@ -1,52 +1,31 @@
 package ru.job4j.todo.repository;
 
 import lombok.AllArgsConstructor;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 import ru.job4j.todo.model.User;
 
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
 @AllArgsConstructor
 public class HbnUserRepository implements UserRepository {
-    private final SessionFactory sf;
+    private final CrudRepository crudRepository;
 
     @Override
     public Optional<User> save(User user) {
-        Optional<User> rsl = Optional.empty();
-        Session session = sf.openSession();
         try {
-            session.beginTransaction();
-            session.save(user);
-            session.getTransaction().commit();
-            rsl = Optional.of(user);
+            crudRepository.run(session -> session.save(user));
         } catch (Exception e) {
-            session.getTransaction().rollback();
-
-        } finally {
-            session.close();
+            return Optional.empty();
         }
-        return rsl;
+        return Optional.of(user);
     }
 
     @Override
     public Optional<User> findByLoginAndPassword(String login, String password) {
-        Optional<User> rsl = Optional.empty();
-        var session = sf.openSession();
-        try {
-            session.beginTransaction();
-            rsl = session.createQuery("FROM User WHERE login = :flogin AND password = :fpassword", User.class)
-                    .setParameter("flogin", login)
-                    .setParameter("fpassword", password)
-                    .uniqueResultOptional();
-            session.getTransaction().commit();
-        } catch (Exception e) {
-            session.getTransaction().rollback();
-        } finally {
-            session.close();
-        }
-        return rsl;
+        return crudRepository.optional("FROM User WHERE login = :flogin AND password = :fpassword",
+                User.class, Map.of("flogin", login, "fpassword", password)
+        );
     }
 }
